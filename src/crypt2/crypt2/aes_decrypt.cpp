edb0cd70662c97a8ce97a22e5fd91ec8539a683b
ableigdev <igor.yarovoy.98@ukr.net>
2018-05-21 10:10:50 +0300

    Refactored mix columns

--- src/crypt2/crypt2/aes_decrypt.cpp
+++ src/crypt2/crypt2/aes_decrypt.cpp
@@ -1,30 +1,15 @@
 #include "aes_decrypt.h"
 
-/*
 #define MixColLoopDecr\
 		cur = *SAr; \
 		cur_8 = unsigned char(cur >> SHIFT_8);\
 		cur_16 = unsigned char(cur >> SHIFT_16);\
 		cur_24 = cur >> SHIFT_24;\
 		cur = unsigned char(cur);\
-		*SAr = (MultiplyBy14[cur] | MultiplyBy11[cur] << SHIFT_24) | (MultiplyBy13[cur] << SHIFT_8) | (MultiplyBy9[cur] << SHIFT_16)\
-				^ MultiplyBy9[cur_8] | (MultiplyBy14[cur_8] << SHIFT_8) | (MultiplyBy11[cur_8] << SHIFT_16) | (MultiplyBy13[cur_8] << SHIFT_24)\
-				^ MultiplyBy13[cur_16] | (MultiplyBy9[cur_16] << SHIFT_8) | (MultiplyBy14[cur_16] << SHIFT_16) | (MultiplyBy11[cur_16] << SHIFT_24)\
-				^ MultiplyBy11[cur_24] | (MultiplyBy13[cur_24] << SHIFT_16) | (MultiplyBy9[cur_24] << SHIFT_24) | (MultiplyBy14[cur_24] << SHIFT_8);\
-		++SAr;
-
-*/
-
-#define MixColLoopDecr\
-		cur = *SAr; \
-		cur_8 = unsigned char(cur >> SHIFT_8);\
-		cur_16 = unsigned char(cur >> SHIFT_16);\
-		cur_24 = cur >> SHIFT_24;\
-		cur = unsigned char(cur);\
-		*SAr = (MultiplyBy14[cur] | MultiplyBy11[cur] << SHIFT_24) | (MultiplyBy13[cur] << SHIFT_8) | (MultiplyBy9[cur] << SHIFT_16)\
-				^ MultiplyBy9[cur_8] | (MultiplyBy14[cur_8] << SHIFT_24) | (MultiplyBy11[cur_8] << SHIFT_8) | (MultiplyBy13[cur_8] << SHIFT_16)\
-				^ MultiplyBy13[cur_16] | (MultiplyBy9[cur_16] << SHIFT_24) | (MultiplyBy14[cur_16] << SHIFT_8) | (MultiplyBy11[cur_16] << SHIFT_16)\
-				^ MultiplyBy11[cur_24] | (MultiplyBy13[cur_24] << SHIFT_24) | (MultiplyBy9[cur_24] << SHIFT_8) | (MultiplyBy14[cur_24] << SHIFT_16);\
+		*SAr = (MultiplyBy14[cur] | (MultiplyBy9[cur] << SHIFT_8) | (MultiplyBy13[cur] << SHIFT_16) | (MultiplyBy11[cur] << SHIFT_24))\
+				^ (MultiplyBy11[cur_8] | (MultiplyBy14[cur_8] << SHIFT_8) | (MultiplyBy9[cur_8] << SHIFT_16) | (MultiplyBy13[cur_8] << SHIFT_24))\
+				^ (MultiplyBy13[cur_16] | (MultiplyBy11[cur_16] << SHIFT_8) | (MultiplyBy14[cur_16] << SHIFT_16) | (MultiplyBy9[cur_16] << SHIFT_24))\
+				^ (MultiplyBy9[cur_24] | (MultiplyBy13[cur_24] << SHIFT_8) | (MultiplyBy11[cur_24] << SHIFT_16) | (MultiplyBy14[cur_24] << SHIFT_24));\
 		++SAr;
 
 DecryptAES::DecryptAES()
@@ -48,20 +33,6 @@ void DecryptAES::encryption(AESDataBlock& block)
 
 inline void DecryptAES::mixColumns()
 {
-	/*
-	for (unsigned char i = 0, temp_ch[4] = { 0 }; i < 16; i += 4)
-	{
-		temp_ch[0] = MultiplyBy14[aesData._ch[i]] ^ MultiplyBy11[aesData._ch[i + 1]] ^ MultiplyBy13[aesData._ch[i + 2]] ^ MultiplyBy9[aesData._ch[i + 3]];
-		temp_ch[1] = MultiplyBy9[aesData._ch[i]] ^ MultiplyBy14[aesData._ch[i + 1]] ^ MultiplyBy11[aesData._ch[i + 2]] ^ MultiplyBy13[aesData._ch[i + 3]];
-		temp_ch[2] = MultiplyBy13[aesData._ch[i]] ^ MultiplyBy9[aesData._ch[i + 1]] ^ MultiplyBy14[aesData._ch[i + 2]] ^ MultiplyBy11[aesData._ch[i + 3]];
-		temp_ch[3] = MultiplyBy11[aesData._ch[i]] ^ MultiplyBy13[aesData._ch[i + 1]] ^ MultiplyBy9[aesData._ch[i + 2]] ^ MultiplyBy14[aesData._ch[i + 3]];
-		
-		for (size_t j = 0; j < 4; ++j)
-		{
-			aesData._ch[i + j] = temp_ch[j];
-		}
-	}
-	*/
 	register unsigned int cur_8;
 	register unsigned int cur;
 	register unsigned int cur_16;
